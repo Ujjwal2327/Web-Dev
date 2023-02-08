eea4591c349e0235b409c745d929c466bfe1cef7
Ujjwal2327 <ujjwal2327@gmail.com>
2023-02-08 12:08:34 +0530

    Modern Chair Project Completed

---
+++ MY CODE/5. JS Basics Language/JS Basics - Class - 1/index.js
@@ -0,0 +1,89 @@
+// print on console window
+console.log('Namaste Duniya version4');
+
+// let a=5;
+// console.log(a);
+
+// const num=6;
+// num = 10;
+// console.log(num);
+
+// let let = 10;
+
+// dynamic typing
+// a="Babbar";
+// console.log(a);
+
+// let lastName = 'Babbar';
+// console.log(lastName);
+// lastName = 15;
+// console.log(lastName);
+
+// let names = ['Rahul', 'Love', 'Sangram'];
+// console.log(names[1]);
+// console.log(names[3]);
+
+// comparision operator
+// console.log(2>3);
+// console.log(3>3);
+// console.log(3>=3);
+// console.log(5>3);
+
+// ternary operator
+// let age = 17;
+// let status = (age>18)? 'Yes Vote' : 'No Vote';
+// console.log(status);
+
+// console.log(true || false);
+// console.log(true || true);
+// console.log(false || 'Babbar');
+// console.log(false || 3);
+// console.log(false || 3);
+
+// let marks = 98;
+// if(marks >= 90){
+
+// }
+
+// for loop
+// for(let i=0; i<5; i++){
+//     console.log('Babbar', i);
+//     console.log('Babbar' + i);
+// }
+
+// while loop
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// do while loop
+// let y=0;
+// do{
+//     console.log(y);
+//     y++;
+// }
+// while(y<10);
+
+// infinite loop
+// while(true){
+//     console.log('Love');
+// }
+
+// let i=0;
+// while(i<Infinity){
+//     console.log('Love');
+// }
+
+// for( ; ;){
+//     console.log('Love');
+// }
+
+// falsy and truthy in and
+console.log(true && true);
+console.log(true && 2);
+console.log('Love' && true);
+console.log(true && 'Love');
+console.log(2 && 'Love');
+console.log('Love' && 2);
